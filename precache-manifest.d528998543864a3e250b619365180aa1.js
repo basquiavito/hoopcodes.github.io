e1d9edd0ffc6d79bcf9b3fa6208a271905cef85a
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2020-06-19 10:07:52 -0400

    Updates

---
+++ precache-manifest.d528998543864a3e250b619365180aa1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c12329a6de07e4f2974c424792ef526a",
+    "url": "/hoopscript.github.io/index.html"
+  },
+  {
+    "revision": "ec581f3ca72f6494f330",
+    "url": "/hoopscript.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c19fdf363d45fb7e579f",
+    "url": "/hoopscript.github.io/static/js/2.723ea6b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hoopscript.github.io/static/js/2.723ea6b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec581f3ca72f6494f330",
+    "url": "/hoopscript.github.io/static/js/main.c3a1adb6.chunk.js"
+  },
+  {
+    "revision": "01b3e1812a6a43eb1aaf",
+    "url": "/hoopscript.github.io/static/js/runtime-main.5b5bba85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hoopscript.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
